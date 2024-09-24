28306d2697e302089f014b63ed64691f180463fc
rewrite0w0 <rewrite0w0@gmail.com>
2024-09-24 15:05:45 +0900

    chore: test 코드 수정한다.

--- tests/generationNumber.test.ts
+++ tests/generationNumber.test.ts
@@ -1,9 +1,9 @@
-import { generationNumber } from "@/util/generationNumber";
+import { generationNumber, MAX_NUMBER } from "@/util/generationNumber";
 
 describe("generationNumber", () => {
   test("생성된 숫자가 0과 100 사이여야 함", () => {
     const number = generationNumber();
     expect(number).toBeGreaterThanOrEqual(0);
-    expect(number).toBeLessThanOrEqual(100);
+    expect(number).toBeLessThanOrEqual(MAX_NUMBER);
   });
 });
